2b449953108862c214e9fdf6ba371301346da083
David Liljefors <david.liljefors@futuregames.nu>
2019-10-28 10:58:42 +0100

    Added textures and Maths library

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -1,16 +1,21 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include<iostream>
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 
 #include "Renderer.h"
 #include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 
 
@@ -27,7 +32,7 @@ int main(void)
 	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(800, 800, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(1200, 600, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -47,10 +52,10 @@ int main(void)
 
 	{
 		float positions[] = {
-		-0.5f,	-0.5f,
-		 0.5f,	-0.5f,
-		 0.5f,	 0.5f,
-		-0.5f,	 0.5f,
+		-0.5f,	-0.5f,	0.0f, 0.0f,
+		 0.5f,	-0.5f,	1.0f, 0.0f,
+		 0.5f,	 0.5f,	1.0f, 1.0f,
+		 -0.5f,  0.5f,	0.0f, 1.0f
 		};
 		unsigned int indices[] =
 		{
@@ -58,18 +63,30 @@ int main(void)
 			2,3,0
 		};
 
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 		
 		VertexBufferLayout layout;
 		layout.Push<float>(2);
+		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
-
+		
 		IndexBuffer ib(indices, 6);
+
+		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.0f, 1.0f, -1.0f, 1.0f);
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		shader.SetUniformMat4f("u_MVP", proj);
+
+
+		Texture texture("res/textures/mild_panic.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
 
 
 		va.Unbind();
@@ -77,21 +94,20 @@ int main(void)
 		ib.Unbind();
 		shader.Unbind();
 
+		Renderer renderer;
+
 		float r = 0.0f;
 		float increment = 0.05f;
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
 			/* Render here */
-			GLCall(glClear(GL_COLOR_BUFFER_BIT));
+			renderer.Clear();
 
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+			renderer.Draw(va, ib, shader);
 
-			va.Bind();
-			ib.Bind();
-
-			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 			if (r > 1.0f)
 			{
 				increment = -0.05f;
@@ -103,10 +119,10 @@ int main(void)
 			r += increment;
 
 			/* Swap front and back buffers */
-			GLCall(glfwSwapBuffers(window));
+			glfwSwapBuffers(window);
 
 			/* Poll for and process events */
-			GLCall(glfwPollEvents());
+			glfwPollEvents();
 		}
 
 		//GLCall(glDeleteProgram(shader));
--- OpenGL/OpenGL/src/Shader.cpp
+++ OpenGL/OpenGL/src/Shader.cpp
@@ -106,12 +106,25 @@ void Shader::Unbind() const
 	GLCall(glUseProgram(0));
 }
 
+void Shader::SetUniform1i(const std::string& name, int value)
+{
+	GLCall(glUniform1i(GetUniformLocation(name), value));
+}
+
+void Shader::SetUniform1f(const std::string& name, float value)
+{
+	GLCall(glUniform1f(GetUniformLocation(name), value));
+}
+
 void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
 {
 	GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
 }
 
-
+void Shader::SetUniformMat4f(const std::string& name, const glm::mat4& matrix)
+{
+	GLCall(glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0]));
+}
 
 unsigned int Shader::GetUniformLocation(const std::string& name)
 {
@@ -119,8 +132,7 @@ unsigned int Shader::GetUniformLocation(const std::string& name)
 		return m_UniformLocationCache[name];
 
 
-	int location;
-	GLCall(location = glGetUniformLocation(m_RendererID, name.c_str()));
+	GLCall(int location = glGetUniformLocation(m_RendererID, name.c_str()));
 	if (location == -1)
 	{
 		std::cout << "Warning: uniform '" << name << "' doesn't exist!" << std::endl;
